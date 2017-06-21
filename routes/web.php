c3b951e77742545cc142ba7edb00abf480c8ad3e
Hartawan\wawan <hartawan_sugihono@yahoo.com>
2017-06-21 13:21:01 +0700

    logout

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,8 @@ Route::get('/', function () {
 });
 
 Auth::routes();
-
+// Route::get('/logout', 'HomeController@logout')->name('logout');
+Route::get('/logout', 'Auth\LoginController@logout');
 // Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/home', 'ProjectListController@index')->name('home');
