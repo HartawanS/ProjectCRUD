73cccf97323c4b412bc7c51131b3a22bd8c3992b
Hartawan\wawan <hartawan_sugihono@yahoo.com>
2017-07-03 15:15:51 +0700

    prikitiuw

--- app/Http/Controllers/masterUserController.php
+++ app/Http/Controllers/masterUserController.php
@@ -33,7 +33,7 @@ class MasterUserController extends Controller
         //validate post data
         $this->validate($request, [
             'name' => 'required',
-            'email' => 'required',
+            'email' => 'bail|required|unique:masteruser',
             'password' => 'required'
         ]);
         
